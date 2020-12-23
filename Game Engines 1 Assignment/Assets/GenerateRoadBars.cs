956160e450a5bf12c37f15a96858fb01dff221b3
Martyna Nieckarz <37311509+martyna-nieckarz@users.noreply.github.com>
2020-12-23 17:48:47 +0000

    Spawning row has its own method now

--- Game Engines 1 Assignment/Assets/GenerateRoadBars.cs
+++ Game Engines 1 Assignment/Assets/GenerateRoadBars.cs
@@ -9,6 +9,8 @@ public class GenerateRoadBars : MonoBehaviour
     public float spaceBetweenBlock = 1.0f;
     public int barSectionNum = 10;
     public float spaceBetweenBarSections = 5.0f;
+
+    private int rowCount = 0;
     public GameObject audioBar;
 
     private float lastBarZ;
@@ -26,51 +28,49 @@ public class GenerateRoadBars : MonoBehaviour
 
             float z = 0;
             for (int i = 0; i < initalRoadLength; i++) {
-                float x = side * (roadWidth/2);
-                for (int sectionNum = 0; sectionNum < barSectionNum/2; sectionNum++) {
-                
-                    GameObject bar = GameObject.Instantiate<GameObject>(audioBar);
-                        bar.transform.position = new Vector3(x, 0, z);
-                    
-                    float hue = (i / (float) initalRoadLength * 10) % 1.0f;
-
-                    bar.GetComponent<Renderer>().material.color =
-                        Color.HSVToRGB(hue, 1, 1);
-
-                    // barScaleZ = bar.transform.localScale.z;
-                    
-                    barPieces.Add(bar);
-
-                    x = x + spaceBetweenBarSections * side;
-                }
                 z = z + spaceBetweenBlock + barScaleZ;
                 lastBarZ = z;
+
+                spawnBarRow(side, 0, z, barScaleZ);
             }
         }
     }
 
-    GameObject spawnBarRow(int side, float x, float y, float z, float barScaleZ) {
+    void spawnBarRow(int side, float y, float z, float barScaleZ) {
+        float x = side * (roadWidth/2);
 
+        // barSection is the "column" of each side
+        for (int sectionNum = 0; sectionNum < barSectionNum/2; sectionNum++) { 
+            GameObject bar = GameObject.Instantiate<GameObject>(audioBar);
+                bar.transform.position = new Vector3(x, 0, z);
+            
+            float hue = (rowCount / (float) initalRoadLength * 10) % 1.0f;
+
+            bar.GetComponent<Renderer>().material.color =
+                Color.HSVToRGB(hue, 1, 1);
+
+            barPieces.Add(bar);
+
+            x = x + spaceBetweenBarSections * side;
+        }
+
+        rowCount = (rowCount + 1) % initalRoadLength;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // float speed = 20.0f;
-        // for (int i = 0; i < barPieces.Count; i++) {
-        //     GameObject bar = barPieces[i];
-        //     Vector3 barPos = bar.transform.position;
-        //     bar.transform.position = new Vector3(barPos.x, barPos.y, barPos.z - Time.deltaTime * speed);
-        // }
+        float speed = 20.0f;
+        for (int i = 0; i < barPieces.Count; i++) {
+            GameObject bar = barPieces[i];
+            Vector3 barPos = bar.transform.position;
+            bar.transform.position = new Vector3(barPos.x, barPos.y, barPos.z - Time.deltaTime * speed);
+        }
         float maxGap = audioBar.transform.localScale.z + spaceBetweenBlock;
 
         print("LBZ: " + lastBarZ);
 
         float lastMovingBarPieceZ = barPieces[barPieces.Count - 1].transform.position.z;
         print("BAR PICE: " + lastMovingBarPieceZ);
-
-        if (lastBarZ - lastMovingBarPieceZ > maxGap) {
-
-        }
     }
 }
