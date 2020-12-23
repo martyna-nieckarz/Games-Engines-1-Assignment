5171798311d79569d1901ac8b1047dd666064453
Martyna Nieckarz <37311509+martyna-nieckarz@users.noreply.github.com>
2020-12-23 03:22:35 +0000

    Cube update

---
+++ Game Engines 1 Assignment/Assets/CubeMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, AudioAnalyzer.smoothedAmplitude * Time.deltaTime * rotSpeed, 0);
+        for (int i = 0; i < elements.Count; i++) {
+            Vector3 ls = elements[i].transform.localScale;
+            ls.y = Mathf.Lerp(ls.y, 1 + (AudioAnalyzer.bands[i] * scale), Time.deltaTime * 3.0f);
+            elements[i].transform.localScale = ls;
+            Vector3 pos = elements[i].transform.position;
+            pos.y = startPositions[i].y + (ls.y / 2);
+            elements[i].transform.position = pos;
+        
+    }
+}
